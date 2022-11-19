ba11838b03979fa2c3b590b4df8b204238cb8cd8
eeduardoliveira <eduardo.ribeiro.oliveira23@gmail.com>
2022-11-19 01:01:02 -0300

    inital

---
+++ README.md
@@ -0,0 +1,34 @@
+# Chrome Clone
+
+A Google Chrome mobile app clone.
+
+## Tools used
+
+* [Axios](https://github.com/axios/axios)
+* [Expo](https://expo.io/)
+* [Google Custom Search API](https://developers.google.com/custom-search/v1/overview)
+* [React Native](https://reactnative.dev/)
+* [React Navigation](https://reactnavigation.org/)
+* [Visual Studio Code](https://code.visualstudio.com/)
+
+## Screenshots
+
+<center>
+<span>
+<img src="assets/home.jpeg" width="250">
+<img src="assets/page.jpeg" width="250">
+</span>
+</center>
+
+## How to use
+
+```bash
+# Clone this repository
+git clone https://github.com/fredcoutinho52/chrome-clone.git
+
+# Install depedencies
+npm install
+
+# Run the app
+npm start
+```
---
+++ src/pages/Main.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { KeyboardAvoidingView, View, StyleSheet, TextInput, Image } from 'react-native'
+
+import api from '../services/api'
+
+import logo from '../assets/logo.png'
+
+export default function Main({ navigation }) {
+    const [search, setSearch] = useState('')
+
+    async function googleSearch() {
+        const apiKey = 'AIzaSyAtiKuzuk3HMWeMFGohgM1m4tbBY6fBMF8'
+        const searchEngineId = 'c0b5c238b6c3b4c89'
+        
+        const response = await api.get(`v1?key=${apiKey}&cx=${searchEngineId}&q=${search}`)
+
+        navigation.navigate('Results', { searchResults: response.data.items })
+    }
+
+    return (
+        <KeyboardAvoidingView style={styles.container}>
+            <Image source={logo} style={styles.image}/>
+
+            <TextInput 
+                style={styles.input} 
+                placeholder="Pesquisar ou digitar endereço da Web"
+                returnKeyType="search"
+                autoFocus={true}
+                value={search}
+                onChangeText={setSearch}
+                onSubmitEditing={googleSearch}
+            />
+        </KeyboardAvoidingView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    image: {
+        width: 230,
+        height: 75,
+        resizeMode: 'stretch',
+    },
+
+    input: {
+        width: 380,
+        height: 45,
+        fontSize: 18,
+        marginTop: 12,
+        paddingHorizontal: 14,
+        borderRadius: 20,
+        backgroundColor: '#f5f5f5'
+    },
+})
